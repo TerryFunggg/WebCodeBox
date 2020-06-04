dbe88eb0a95f68904d38a00bcf80b470c08627d8
TerryFunggg <terryyessfung@gmail.com>
2020-06-05 00:31:38 +0800

    upload some project

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
-const card = document.querySelector(".card");
+const showcaseOnClick = e =>{
+    console.log(e.childNodes);
+    const iframe =  this.childNodes[1];
+   window.location = iframe.src;
+}
+
+const collection = document.getElementsByClassName("card");
+let showcases = [...collection];
+
+showcases.forEach(element => {
+    element.addEventListener("click",function(){
+        const iframe =  this.childNodes[1];
+        window.location = iframe.src;
+    });
+});
 document.getElementById('refresh').addEventListener('click',function(e){
     alert("Sorry! Still developing")
 });
 
-card.addEventListener("click",function(e){
-   const iframe =  this.childNodes[1];
-   window.location = iframe.src;
-})
