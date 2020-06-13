6d71f7e86314b23fe84822d254b1e1dd4b45c0ed
TerryFunggg <terryyessfung@gmail.com>
2020-06-13 18:04:03 +0800

    change style

--- js/index.js
+++ js/index.js
@@ -9,10 +9,10 @@ const showcases_box = document.getElementById("showcases");
 fetchShowcase();
 
 // Get footer year
-document.getElementById('year').innerHTML = new Date().getFullYear();
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
 document.getElementById("refresh").addEventListener("click", function (e) {
-    showcases_box.innerHTML = '';
+    showcases_box.innerHTML = "";
     fetchShowcase();
 });
 
@@ -28,10 +28,9 @@ async function fetchShowcase() {
     initTimeLine(data);
 
     const collection = document.getElementsByClassName("card");
-   
+
     // add onClick listener for each showcase
     addListenerForCards([...collection]);
-    
 }
 
 function insertShowcase(showcases) {
@@ -49,28 +48,29 @@ function insertShowcase(showcases) {
     });
 }
 
-function initTimeLine(data){
+function initTimeLine(data) {
     console.log(data);
-    let list = data.slice()
-    .sort((a,b) => new Date(b.createAt) - new Date(a.createAt));
+    let list = data
+        .slice()
+        .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
     const timeline = document.getElementById("timeline");
-    let html = '';
+    let html = "";
 
     for (let index = 0; index < list.length; index++) {
-        let position = 'left';
+        let position = "left";
         let tag = "info";
-        
-        if(index % 2 == 0) position = 'right'
-        if((list[index].tag).includes('css')) tag = "danger"
+
+        if (index % 2 == 0) position = "right";
+        if (list[index].tag.includes("css")) tag = "danger";
         html += `
         <div class="timeline-item timeline-item-${position}">
             <div class="timeline-content">
                 <a href="${list[index].link}"><h4>${list[index].name}</h4></a>
-                <p>post at ${list[index].createAt}</p>
+                <p>post at ${list[index].createdAt}</p>
                 <span class="badge badge-${tag}">${list[index].tag}</span>
             </div>
         </div>
-        `
+        `;
     }
     timeline.innerHTML = html;
 }
@@ -86,12 +86,11 @@ function shuffleItems(data) {
         .map((x) => x.data);
 }
 
-function addListenerForCards(showcases){
-showcases.forEach((element) => {
-    element.addEventListener("click", function () {
-        const iframe = this.childNodes[1];
-        window.location = iframe.src;
+function addListenerForCards(showcases) {
+    showcases.forEach((element) => {
+        element.addEventListener("click", function () {
+            const iframe = this.childNodes[1];
+            window.location = iframe.src;
+        });
     });
-});
 }
-
