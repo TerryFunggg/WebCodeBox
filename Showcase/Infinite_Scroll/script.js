1f81338c31b19c3a5246ce1203950ac51faf20d1
TerryFunggg <terryyessfung@gmail.com>
2020-06-21 21:58:11 +0800

    add new project

---
+++ Showcase/Infinite_Scroll/script.js
@@ -0,0 +1,72 @@
+const postsContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 5;
+let page = 1;
+
+async function getPost(){
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+    const data = await res.json();
+    return data;
+}
+
+//Show posts in dom
+async function showPosts(){
+    const posts = await getPost();
+
+   posts.forEach(post => {
+       const postEl = document.createElement('div');
+       postEl.classList.add('post');
+       postEl.innerHTML = `
+       <div class="post-info">
+           <h2 class="post-title">${post.title}</h2>
+           <div class="post-body">
+              ${post.body}
+           </div>
+       </div>
+       `;
+
+       postsContainer.appendChild(postEl)
+   })
+}
+
+function showLoading(){
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() =>{
+            page++;
+            showPosts();
+        }, 300);
+    }, 1000)
+}
+
+function filterPosts(e){
+    const term = e.target.value.toUpperCase();
+    const posts = document.querySelectorAll('.post');
+
+    posts.forEach(post=>{
+        const title = post.querySelector('.post-title').innerText;
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
+        if(title.indexOf(term) > -1 || body.indexOf(term) > -1){
+            post.style.display ='flex';
+        }else{
+            post.style.display = 'none';
+        }
+    })
+}
+
+showPosts();
+
+window.addEventListener('scroll', ()=> {
+    const {scrollTop, scrollHeight,clientHeight} = document.documentElement;
+    
+    if(scrollTop + clientHeight >= scrollHeight - 5){
+        showLoading();
+    }
+})
+
+filter.addEventListener('input', filterPosts);
